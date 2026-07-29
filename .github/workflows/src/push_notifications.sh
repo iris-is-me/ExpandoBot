755a51a1c805741b36eb16dbf362d4dc606c39cb
Janford-28719 <janford28719@gmail.com>
2026-07-29 15:30:45 +0800

    Modified github push notification webhook

--- .github/workflows/src/push_notifications.sh
+++ .github/workflows/src/push_notifications.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -f "./assets/push_notification_payload.json" ]; then
+if [ ! -f ".github/workflows/src/assets/push_notification_payload.json" ]; then
     echo "Error: file does not exist" >&2
     exit 1
 fi
 
 curl -X POST "$DISCORD_WEBHOOK_URL" \
   -H "Content-Type: application/json" \
-  --data @./assets/push_notification_payload.json
+  --data @.github/workflows/src/assets/push_notification_payload.json
