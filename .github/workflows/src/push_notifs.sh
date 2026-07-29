23372d8988c3350185608377709a2674c1de2268
Janford-28719 <janford28719@gmail.com>
2026-07-29 15:10:53 +0800

    Modified parameters used in `curl`

--- .github/workflows/src/push_notifs.sh
+++ .github/workflows/src/push_notifs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-clear
-
 curl -X POST "$DISCORD_WEBHOOK_URL" \
   -H "Content-Type: application/json" \
-  --data-binary @./push_notifs_payload.json
+  --data @./push_notifs_payload.json
