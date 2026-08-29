80ae88961bc07cfb61a6703ad368a6cbe903aedc
Janford-28719 <janford28719@gmail.com>
2026-08-29 10:39:11 +0800

    Added plugin context of logger and bot

--- src/core/plugin.py
+++ src/core/plugin.py
@@ -2,7 +2,20 @@ import logging
 import discord
 
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from logging import Logger
 
+from client import Bot
+
+@dataclass(slots=True)
+class PluginContext():
+    """
+    Base class for all plugin contexts.
+
+    This class contains the context a plugin may use to function.
+    """
+    bot: Bot
+    logger: Logger
 
 class Plugin(ABC):
     """
@@ -10,7 +23,18 @@ class Plugin(ABC):
 
     Subclasses override lifecycle hooks and use typed properties for framework access.
     """
+    def __init__(self, context: PluginContext) -> None:
+        super().__init__()
+        self._context = context
     
+    @property
+    def bot(self) -> Bot:
+        return self._context.bot
+    
+    @property
+    def logger(self) -> Logger:
+        return self._context.logger
+
     @abstractmethod
     async def on_load(self) -> None:
         """Called after the plugin object is created and migrations are available."""
